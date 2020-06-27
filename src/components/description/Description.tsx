baa59a740a1dbe41f255136abd3341eafd7149ae
Neil Berg <neil.berg14@gmail.com>
2020-06-27 15:50:06 -0700

    fix font name

--- src/components/description/Description.tsx
+++ src/components/description/Description.tsx
@@ -10,7 +10,7 @@ export const Description = (props: DescriptionProps) => (
 );
 
 const StyledDescription = styled.p`
-  font-family: 'Sanburn', sans-serif;
+  font-family: 'Sarabun', sans-serif;
   color: #4e4b4b;
   font-size: 16px;
   margin: 24px 0;
